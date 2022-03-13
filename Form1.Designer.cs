93651b73c4fbc2465f90c8c339645bb57d160ae8
Saaid Faraz <ac117413@avcol.school.nz>
2022-03-14 12:03:17 +1300

    Created level one

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.question = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.RadioButton();
+            this.option2 = new System.Windows.Forms.RadioButton();
+            this.option3 = new System.Windows.Forms.RadioButton();
+            this.tickBox = new System.Windows.Forms.Button();
+            this.engWord = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // question
+            // 
+            this.question.BackColor = System.Drawing.Color.RosyBrown;
+            this.question.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question.Location = new System.Drawing.Point(100, 100);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(800, 40);
+            this.question.TabIndex = 2;
+            this.question.Text = "This Programme will ask you Multiple choice questions, Press the checkbox to cont" +
+    "inue";
+            this.question.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option1
+            // 
+            this.option1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(158)))), ((int)(((byte)(159)))));
+            this.option1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1.Location = new System.Drawing.Point(190, 219);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(100, 50);
+            this.option1.TabIndex = 3;
+            this.option1.TabStop = true;
+            this.option1.Text = "Example 1";
+            this.option1.UseVisualStyleBackColor = false;
+            this.option1.CheckedChanged += new System.EventHandler(this.option1_CheckedChanged);
+            // 
+            // option2
+            // 
+            this.option2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(158)))), ((int)(((byte)(159)))));
+            this.option2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2.Location = new System.Drawing.Point(459, 219);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(100, 50);
+            this.option2.TabIndex = 4;
+            this.option2.TabStop = true;
+            this.option2.Text = "Example 2";
+            this.option2.UseVisualStyleBackColor = false;
+            this.option2.CheckedChanged += new System.EventHandler(this.option2_CheckedChanged);
+            // 
+            // option3
+            // 
+            this.option3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(158)))), ((int)(((byte)(159)))));
+            this.option3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3.Location = new System.Drawing.Point(712, 219);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(100, 50);
+            this.option3.TabIndex = 5;
+            this.option3.TabStop = true;
+            this.option3.Text = "Example 3";
+            this.option3.UseVisualStyleBackColor = false;
+            this.option3.CheckedChanged += new System.EventHandler(this.option3_CheckedChanged);
+            // 
+            // tickBox
+            // 
+            this.tickBox.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.tickBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tickBox.Location = new System.Drawing.Point(472, 347);
+            this.tickBox.Name = "tickBox";
+            this.tickBox.Size = new System.Drawing.Size(75, 57);
+            this.tickBox.TabIndex = 6;
+            this.tickBox.Text = "✓";
+            this.tickBox.UseVisualStyleBackColor = false;
+            this.tickBox.Click += new System.EventHandler(this.tickBox_Click);
+            // 
+            // engWord
+            // 
+            this.engWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.engWord.Location = new System.Drawing.Point(100, 150);
+            this.engWord.Name = "engWord";
+            this.engWord.Size = new System.Drawing.Size(800, 80);
+            this.engWord.TabIndex = 7;
+            this.engWord.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(245)))), ((int)(((byte)(219)))));
+            this.ClientSize = new System.Drawing.Size(960, 457);
+            this.Controls.Add(this.engWord);
+            this.Controls.Add(this.tickBox);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.question);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -44,6 +129,12 @@
         }
 
         #endregion
+        private System.Windows.Forms.Label question;
+        private System.Windows.Forms.RadioButton option1;
+        private System.Windows.Forms.RadioButton option2;
+        private System.Windows.Forms.RadioButton option3;
+        private System.Windows.Forms.Button tickBox;
+        public System.Windows.Forms.Label engWord;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,138 @@ namespace Computer_Programme
 {
     public partial class Form1 : Form
     {
+        int flag = 0;
+        Random rnd = new Random();
+        int index = 0;
+        public int score = 0;
+        int optionNumber = 0;
+        public int meaningIndex { get; private set; }
+        string[] maoriWords = { "Aotearoa ", "aroha", "awa ", "haka ", "hangi ", "hapu", "hīkoi", "hui ", "iti", "iwi ", "kai ",
+            "karakia ", "kaumatua ", "kauri ", "kiwi ", "koha ", "kōhanga reo", "mahi ", "mana ", "manuhiri ", "Māori ", "marae ",
+            "maunga", "moa", "moana ", "motu", "nui ", "pā ", "Pākehā ", "pounamu ", "puku ", "rangatira ", "taihoa ", "tama ", 
+            "tamāhine ", "tamariki ", "tāne ", "tangi ", "taonga ", "tapu ", "te reo Māori ", "tipuna", "tuatara ", "wahine ",
+            "wai", "waiata ", "waka ", "whaikōrero ", "whakapapa ", "whānau ", "whenua " };
+        string[] meaning = { "(New Zealand, long white cloud)", "(love)", "(river)", "(generic term for Māori dance. )",
+            "(traditional feast prepared in earth oven)", " (clan, sub-tribe; to be born )", "(walk)", " (gathering, meeting)", 
+            " (small)", " (tribe)", "(food)", "(prayer)", "(elder)", "(large native conifer)", "(native flightless bird)", 
+            "(gift, present (usually given by guest to hosts))", 
+            " (language nest, Maori immersion pre-school (0 to 4 years))", "(work or activity)", "(prestige, reputation)", 
+            "(guests, visitors)", "(indigenous inhabitants of New Zealand,\n the language of the indigenous inhabitants of New Zealand)",
+            "(the area for formal discourse in front of a meeting house)", " (mountain)",
+            "(extinct large flightless bird)", "(sea)", "(island)", "(large, many, big)", " (hill fort)", 
+            "(New Zealander of non-Māori descent, usually European)", " (greenstone, jade)", " (belly, stomach)", 
+            "(person of chiefly rank, boss )", "(to delay, to wait, to hold off to \n allow maturation of plans etc. )",
+            "(son, young man, youth)", " (daughter)", " (children)", " (man, husband, men, husbands)", "(funeral)", 
+            "(treasured possessions or cultural items, anything precious)", "(sacred, not to be touched, to be avoided because sacred)", 
+            " (the Māori Language)", "(ancestor )", " (reptiles endemic to New Zealand, the order Rhynchocephalia)",
+            "(woman, wife)", " (water)", "(song or chant)", "(canoe, canoe group)", " (the art and practise of speech-making )", 
+            "(genealogy, to recite genealogy )", "(extended family)", "(land, homeland)" };
+        int[] usedNumbers = new int[30];
+        int[] optionIndex = new int[3];
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public void Refresh()
+        {
+
+
+            optionIndex[0] = uniqueRnd();
+            optionIndex[1] = uniqueRnd();
+            optionIndex[2] = uniqueRnd();
+            meaningIndex = rnd.Next(3);
+            engWord.Text = meaning[optionIndex[meaningIndex]];
+            option1.Text = maoriWords[optionIndex[0]];
+            option2.Text = maoriWords[optionIndex[1]];
+            option3.Text = maoriWords[optionIndex[2]];
+
+
+
+        }
+        public void CheckScore()
+        {
+            if (optionNumber-1 == meaningIndex && optionNumber != 0)
+            { score++; }
+          
+        }
+
+        public int uniqueRnd()
+        {
+
+            while (true)
+            {
+                bool unique = true;
+                int rndNumber = rnd.Next(50);
+
+                foreach (int usedNumber in usedNumbers)
+                {  
+                    if (rndNumber == usedNumber)
+                    { 
+                        unique = false;
+                    }
+                }
+                if (unique == true)
+                 {
+                    usedNumbers[index] = rndNumber;
+                    index++;
+                    return rndNumber;
+                    ;
+                }
+            }
+            
+        }
+
+
+        private void option1_CheckedChanged(object sender, EventArgs e)
+        {
+            optionNumber = 1;
+        }
+
+
+        private void option2_CheckedChanged(object sender, EventArgs e)
+        {
+            optionNumber = 2;
+        }
+
+        private void option3_CheckedChanged(object sender, EventArgs e)
+        {
+            optionNumber = 3;
+        }
+
+        private void tickBox_Click(object sender, EventArgs e)
+        {
+            question.Text = "Choose the word with the same meaning. Press the checkbox to continue.";
+            if (flag >= 1)
+            {
+                CheckScore();
+            }
+            if (flag == 10)
+            {
+                if (score == 10)
+                {
+                    MessageBox.Show("👏Wow! 🎊 You got all correct!🎉", "Congratulations!");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show($"You got {score} out of 10", "Congratulations");
+                    Close();
+                }
+            }
+
+            if (flag < 10)
+            {
+                Refresh();
+            }
+            flag++;
+        }
+
+
+
+
     }
 }
