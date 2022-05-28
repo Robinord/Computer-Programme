bf8fde3e2b168d5bde281e1d0d0a8cad86eafc4c
Saaid Faraz <farazsaaid@gmail.com>
2022-05-29 10:54:23 +1200

    Updated Pseudocode for form2

--- Form2.cs
+++ Form2.cs
@@ -1,13 +1,16 @@
 ﻿/*
 This programme will ask the user for the maori word corresponding to the meaning displayed
-list of Maori Words = string[50 values]
-list of alternate Maori words = string[50 values]
-list of the meanings = string [50 values]
-Timer Event()
-    If 80 seconds passed;
-        Exit Method()
-if close button pressed:
-    Exit the whole programme
+Define this form as an object called frm2 for the frm class()
+When Form Loads()
+     Hide textbox
+Refresh screen Method()
+    Meaning = Meanings[Call Unique Random Number method from the frm class(max value: 49)]
+Check Score Method()
+    If Text Entered is the maori word or the alternative maori word which corresponds to the meaning:
+        Score + 1
+        Display "Correct Answer"
+    Else
+        Display the right answer
 if tickBox pressed:
     Call Run method()
 if Enter key pressed:
@@ -15,26 +18,22 @@ if Enter key pressed:
     Call Run Method()
 Run Method()
     Call Refresh screen method()
-    Call Score method()
+    Call check Score method()
     if used 10 times:
         Call Exit Method()
     Update the progress Label
+Timer Event()
+    If 80 seconds passed;
+        Exit Method()
 Exit Method()
     Show score
-    Exit Application
-Refresh screen Method()
-    Meaning = Meanings[Call Unique Random Number method(max value: 49)]
-Check Score Method()
-    If Text Entered is the maori word or the alternative maori word which corresponds to the meaning:
-        Score + 1
-Unique Random Number Method()
-    While(Until Returns)
-        Generate Random Number(below 50)
-        if Random Number already used:
-            Generate another Random Number(below 50)
-        else:
-            Save Random Number as Used Numbers
-            Return Random Number
+    Ask user if they want to restart
+        If yes:
+            Open Form1
+        If No:
+            Exit the whole programme
+if close button pressed:
+    Exit the whole programme
  */
 using System;
 using System.Collections.Generic;
@@ -52,7 +51,13 @@ namespace Computer_Programme
     {
         Frm frm2 = new Frm(80, 0, 0, 0, new int[10]);
         Random rnd = new Random();
-        string answer;
+
+        public Form2() => InitializeComponent();//initializing this form
+
+        private void Form2_Load(object sender, EventArgs e) => input.Hide();
+
+        private void input_TextChanged(object sender, EventArgs e) => frm2.answer = input.Text;//to keep account of the text entered
+
         public void RefreshScreen()//made to refresh the meaning displayed on screen
         {
             frm2.MeaningIndex = frm2.UniqueRnd(49);//made the value a variable because it is used again in Check Score method
@@ -63,12 +68,12 @@ namespace Computer_Programme
         public void CheckScore()
         {   
             timer.Stop();//to stop the timer while the user reads if they got the answer right or not
-            question.Text = "Press checkbox or enter key to continue";
-            if (answer == null)//to noot run into error if user doesn't enter anything
+            input.Enabled = false;
+            if (frm2.answer == null)//to noot run into error if user doesn't enter anything
             {
-                answer = "  ";
+                frm2.answer = "  ";
             }
-            if (answer.ToLower() == (Frm.MaoriWords[frm2.MeaningIndex]).ToLower() || answer.ToLower() == (Frm.CorrectionWords[frm2.MeaningIndex]).ToLower())//to check if the answer entered is right
+            if (frm2.answer.ToLower() == (Frm.MaoriWords[frm2.MeaningIndex]).ToLower() || frm2.answer.ToLower() == (Frm.CorrectionWords[frm2.MeaningIndex]).ToLower())//to check if the answer entered is right
             {
                 frm2.Score++;
                 engWord.ForeColor = Color.LightGreen;
@@ -80,29 +85,11 @@ namespace Computer_Programme
                 engWord.ForeColor = Color.LightSalmon;
                 engWord.Text = $"Incorrect answer! Correct answer was: {Frm.MaoriWords[frm2.MeaningIndex]}";
             }   // just to let the user know that they got it uncorrect and what the real answer is
-            input.Enabled = false;
+            question.Text = "Press checkbox or enter key to continue";
 
         }
 
-      
-        public void Exit()
-        {
-            timer.Stop();
-            string message = $"You got {frm2.Score} out of 10, would you like to restart?";
-            string caption = "Congratulations";
-            var result = MessageBox.Show(message, caption,MessageBoxButtons.YesNo,MessageBoxIcon.Question);
-                                         
-            if (result == DialogResult.Yes)
-            {
-                this.Hide();//hides the current for when the new form opens
-                Form1 form = new Form1();
-                form.Show();//opens new form;
-            }
-            else
-            {
-                Application.Exit();//Exits the whole programme including the previous form
-            }
-        }
+
         public void Run()
         {
             question.Text = "Choose the word with the same meaning. Press the checkbox to continue.";
@@ -134,12 +121,9 @@ namespace Computer_Programme
             frm2.Counter++;//updates the counter for the number of time the TickBox has been pressed
         }
 
-        public Form2() => InitializeComponent();//initializing this form
+       
 
-        private void input_TextChanged(object sender, EventArgs e) => answer = input.Text;//to keep account of the text entered
-
-        private void Form2_FormClosing(object sender, FormClosingEventArgs e) => Application.Exit();//so that the previous hidden form 1 closes along aswell
-
+        
         private void tickBox_Click(object sender, EventArgs e) => Run();
 
         private void input_KeyDown(object sender, KeyEventArgs e)
@@ -162,9 +146,26 @@ namespace Computer_Programme
             frm2.SecsLeft--;
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+        public void Exit()
         {
-            input.Hide();
+            timer.Stop();
+            string message = $"You got {frm2.Score} out of 10, would you like to restart?";
+            string caption = "Congratulations";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                this.Hide();//hides the current for when the new form opens
+                Form1 form = new Form1();
+                form.Show();//opens new form;
+            }
+            else
+            {
+                Application.Exit();//Exits the whole programme including the previous form
+            }
         }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e) => Application.Exit();//so that the previous hidden form 1 closes along aswell
+
     }
 }
